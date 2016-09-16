6d9dccf13919d71826aa9e2a6902c84bb5c54653
avizcaino <avizcainoluna@gmail.com>
2016-09-16 19:58:37 +0200

    actors list + background-image behavior

---
+++ src/core/ui/behaviors/background-image.ts
@@ -0,0 +1,17 @@
+/**
+ * Created by alexvizcaino on 16/9/16.
+ */
+import {autoinject} from "aurelia-dependency-injection";
+import {bindable} from "aurelia-templating";
+import {customAttribute} from "aurelia-templating";
+
+@customAttribute('background-image')
+@autoinject
+export class BackgroundImage{
+  constructor(private _element:Element){}
+
+  valueChanged(newValue){
+    console.log(newValue);
+    (<HTMLElement>this._element).style.backgroundImage = 'url(' + newValue +')';
+  }
+}
--- src/core/ui/index.ts
+++ src/core/ui/index.ts
@@ -3,5 +3,5 @@
  */
 import {FrameworkConfiguration} from "aurelia-framework";
 export function configure(fc:FrameworkConfiguration) {
-  fc.globalResources(['./command/command-ext', './behaviors/on-enter', './elements/searcher-toolbar']);
+  fc.globalResources(['./command/command-ext', './behaviors/on-enter', './behaviors/background-image', './elements/searcher-toolbar']);
 }
